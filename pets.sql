e44d6127fffd8059dd07ac9813f3d5691c3b09bc
itowlson <ivan.towlson@fermyon.com>
2026-08-12 12:51:39 +1200

    SQLite!

---
+++ pets.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS pets(name TEXT, age INTEGER);
+INSERT INTO pets(name, age) VALUES ('Hobbes', 4);
--- the-dep/src/lib.rs
+++ the-dep/src/lib.rs
@@ -7,10 +7,27 @@ struct KVLover;
 
 impl exports::impo::impo::i_love_kv::Guest for KVLover {
     async fn get_the_kv(k: _rt::String,) -> Option<_rt::Vec::<u8>> {
-        match spin_sdk::variables::get(&k).await {
-            Ok(v) => Some(v.into()),
-            Err(e) => Some(e.to_string().into()),
-        }
+        let vtest = match spin_sdk::variables::get(&k).await {
+            Ok(v) => v,
+            Err(e) => { return Some(e.to_string().into()); }
+        };
+
+        let Ok(conn) = spin_sdk::sqlite::Connection::open_default().await else {
+            return Some("sqlite conn woe!\n".into());
+        };
+
+        let Ok(qr) = conn.execute("select * from pets", []).await else {
+            return Some("sqlite exec woe!\n".into());
+        };
+
+        let Ok(resset) = qr.collect().await else  {
+            return Some("sqlite collect woe!\n".into());
+        };
+
+        let res_str = format!("{resset:?}");
+
+        Some((format!("vtest = '{vtest}' and resset = '{res_str}'")).into())
+
         // let store = spin_sdk::key_value::Store::open("fie").await.unwrap();
         // store.get(&k).await.unwrap()
     }
